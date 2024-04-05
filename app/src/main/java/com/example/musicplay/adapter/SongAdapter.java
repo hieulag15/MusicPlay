d9ca59862f1e9fee75499a97074a7a7728ccf51d
tamnguyen1411 <chuotvip233@gmail.com>
2024-04-05 22:05:54 +0700

    thêm adapter

---
+++ app/src/main/java/com/example/musicplay/adapter/SongAdapter.java
@@ -0,0 +1,55 @@
+package com.example.musicplay.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.bumptech.glide.Glide;
+import com.example.musicplay.domain.Song;
+import com.example.musicplayer.R;
+
+import java.util.List;
+
+public class SongAdapter extends ArrayAdapter<Song> {
+    private Context context;
+
+    private int resource;
+    public SongAdapter(Context context, int resource, List<Song> songs) {
+        super(context, resource,songs);
+        this.context = context;
+        this.resource = resource;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view = convertView;
+        if (view == null) {
+            LayoutInflater inflater = LayoutInflater.from(context);
+            view = inflater.inflate(resource, parent, false);
+        }
+
+        Song song = getItem(position);
+
+        ImageView albumArt = view.findViewById(R.id.imgSong);
+        Glide.with(convertView.getContext())
+                .load(song.getImage())
+                .into(albumArt);
+
+
+        TextView songTitle = view.findViewById(R.id.tvSongName);
+        songTitle.setText(song.getName());
+
+        TextView artistName = view.findViewById(R.id.tvSinger);
+        artistName.setText(song.getSinger());
+
+        return view;
+    }
+}
